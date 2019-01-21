f5fd1a9413c45a7b6c712cd75818e53ed3d7b382
Jonas Bakken <jonasxb@gmail.com>
2019-01-22 00:20:51 +0100

    Add gh-pages branch

--- pkg/module.js
+++
@@ -1,7 +0,0 @@
-export * from "./gl-matrix";
-export * from "./camera";
-export * from "./controller";
-export * from "./light";
-export * from "./scene";
-export * from "./shader";
-export * from "./vertexarray";
